60731dae48658835101e66014d16803e88760e05
Ejabu <muhammadfahreza@gmail.com>
2021-06-06 23:22:50 +0700

    add: Notification Area Icon and Menu

--- TrayApp/MainWindow.xaml.cs
+++ TrayApp/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace TrayApp
 {
@@ -20,9 +23,44 @@ namespace TrayApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        NotifyIcon TrayIcon = new NotifyIcon();
+
         public MainWindow()
         {
             InitializeComponent();
+            var contextMenuStrip = new ContextMenuStrip();
+
+            // Toggle Show Menu Item
+            var toggleShowMenuItem = new ToolStripMenuItem
+            {
+                Text = "Show",
+            };
+
+            toggleShowMenuItem.Click += (e, s) =>
+            {
+                Debug.WriteLine("something");
+            };
+
+            // Exit Menu Item
+            var exitMenuItem = new ToolStripMenuItem
+            {
+                Text = "Exit",
+            };
+
+            toggleShowMenuItem.Click += (e, s) =>
+            {
+                Debug.WriteLine("something");
+            };
+
+
+            contextMenuStrip.Items.Add(toggleShowMenuItem);
+            contextMenuStrip.Items.Add(new ToolStripSeparator());
+            contextMenuStrip.Items.Add(exitMenuItem);
+            TrayIcon.Icon = new Icon(@"Resources/Icon.ico");
+            TrayIcon.Visible = true;
+            TrayIcon.Text = "Tray Application";
+
+            TrayIcon.ContextMenuStrip = contextMenuStrip;
         }
     }
 }
